cacc3b48558fe007afbd19f03455edfab769b6f9
Mac <vettrov78@gmail.com>
2024-04-16 09:25:20 +0300

    Исправления наставника

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import WelcomePage from '../../pages/welcome-page/welcome-screen';
-import SignInPage from '../../pages/sign-in-page/sign-in';
-import MyListPage from '../../pages/my-list-page/my-list';
-import FilmPage from '../../pages/film-page/film';
-import AddReviewPage from '../../pages/add-review-page/add-review';
-import PlayerPage from '../../pages/player-page/player';
+import WelcomePage from '../../pages/welcome-page/welcome-page';
+import SignInPage from '../../pages/sign-in-page/sign-in-page';
+import MyListPage from '../../pages/my-list-page/my-list-page';
+import FilmPage from '../../pages/film-page/film-page';
+import AddReviewPage from '../../pages/add-review-page/add-review-page';
+import PlayerPage from '../../pages/player-page/player-page';
 import PrivateRoute from '../private-route/private-route';
 import { AppRoute, AuthorizationStatus } from '../../const';
-import NotFoundPage from '../../pages/not-found-page/not-found';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
 
 type AppScreenProps = {
   filmCardsNumber: number;
@@ -31,11 +31,8 @@ function App({ filmCardsNumber }: AppScreenProps): JSX.Element {
         <Route path={AppRoute.Film} element={<FilmPage />} />
         <Route path={AppRoute.Review} element={<AddReviewPage />} />
         <Route path={AppRoute.Player} element={<PlayerPage />} />
-<<<<<<< HEAD
         <Route path='*' element={<h1>Ошибка 404. Страница не существует.</h1>} />
-=======
         <Route path='*' element={<NotFoundPage />} />
->>>>>>> module3-task1
       </Routes>
     </BrowserRouter>
   );
